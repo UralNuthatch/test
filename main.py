6c696eeca399b95f7f5c2a59251fe5865a89b7fd
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-20 13:09:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import logging
 import google.generativeai as genai
 from aiogram import Dispatcher, Bot
-from aiogram.types import Message
-from aiogram.filters import CommandStart
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiohttp import web
 from os import getenv
+from handlers import user_handlers, other_handlers
 
 
 
@@ -15,18 +14,23 @@ API_KEY = getenv('API_KEY')
 
 # Настройки веб-сервера
 WEB_SERVER_HOST = "https://nuthatch.onrender.com"
-# Порты сервера: 8300-8499
+# Порты сервера: по умолчанию 8080
 WEB_SERVER_PORT = 8080
 # Путь к маршруту вебхука, по которому Telegram будет отправлять запросы
 WEBHOOK_PATH = ""
 # Базовый URL-адрес вебхука, который будет исп-ся для создания URL-адреса вебхука для Telegram
 BASE_WEBHOOK_URL = f"{WEB_SERVER_HOST}{WEBHOOK_PATH}"
-# На сервере только IPv6 (аналог ip4: 0.0.0.0)
+# На сервере ip4: 0.0.0.0
 WEBAPP_HOST = "0.0.0.0"
 
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
+
+
+# Регистрируем роутеры в диспетчере
+dp.include_router(user_handlers.router)
+dp.include_router(other_handlers.router)
 
 
 genai.configure(api_key=API_KEY)
@@ -36,18 +40,6 @@ model = genai.GenerativeModel('gemini-pro')
 async def on_startup(bot: Bot) -> None:
     await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}")
 
-@dp.message(CommandStart)
-async def get_process_start(message: Message):
-    await message.answer("Start been activated. Hello!")
-
-
-@dp.message()
-async def get_send_text(message: Message):
-    try:
-        responce = model.generate_content(message.text)
-        await message.answer(responce.text)
-    except Exception as ex:
-        await message.answer("Error")
 
 
 def main():
