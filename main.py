7b44652e109be9e91fcbc5ec7696c4f31dce3738
UralNuthatch <146403814+UralNuthatch@users.noreply.github.com>
2023-12-20 09:37:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
-import sys
-from os import getenv
-from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
+import google.generativeai as genai
+from aiogram import Dispatcher, Bot
 from aiogram.types import Message
-from aiogram.types import ContentType
-from aiogram import F
+from os import getenv
 
 
-# Создаем объекты бота и диспетчера
-bot = Bot(token=getenv("BOT_TOKEN"))
+BOT_TOKEN = getenv('BOT_TOKEN')
+API_KEY = getenv('API_KEY')
+
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-# Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
-async def process_start_command(message: Message):
-    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
-# Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
-async def process_help_command(message: Message):
-    await message.answer('Напиши мне что-нибудь и в ответ\nя пришлю тебе твое сообщение')
+genai.configure(api_key=API_KEY)
+model = genai.GenerativeModel('gemini-pro')
 
 
-
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения
-# кроме команд "/start" и "/help"
 @dp.message()
-async def send_echo(message: Message):
+async def get_send_text(message: Message):
     try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.reply(text = 'Данный тип апдейтов не поддерживается методом send_copy')
+        responce = model.generate_content(message.text)
+        await message.answer(responce.text)
+    except Exception as ex:
+        await message.answer("Error")
 
 
 if __name__ == '__main__':
